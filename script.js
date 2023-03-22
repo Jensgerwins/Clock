7301060b025e1ca7c3107834deb1e1f5d7da7c2f
Jens gerwins <Jens.gerwins@gmail.com>
2023-03-22 20:28:55 +0100

    Digital uhr

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const digStd =document.getElementsByClassName("digital-Std"); 
-const digMin =document.getElementsByClassName("digital-Min"); 
-const digSec =document.getElementsByClassName("digital-Sec"); 
+const digStd =document.getElementById("digital-Std"); 
+const digMin =document.getElementById("digital-Min"); 
+const digSec =document.getElementById("digital-Sec"); 
 
-let digitalstd =0;
-let digitalmin =0;
-let digitalsec =0;
+function DigUhr() {
+const d =new Date();
+let digitalstd =d.getHours();
+let digitalmin =d.getMinutes();
+let digitalsec =d.getSeconds();
+
+digStd.innerText = nummerToStr(digitalstd);
+digMin.innerText = nummerToStr(digitalmin);
+digSec.innerText = nummerToStr(digitalsec);
+
+}
+
+
+function nummerToStr (z) {
+    let time = "0" + z.toString();
+    return time.slice(-2);
+}
+
+
+DigUhr();
+setInterval(DigUhr, 1000);
 
