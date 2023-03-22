be669efb93021bd5eea808696295e215aa320378
Jens gerwins <Jens.gerwins@gmail.com>
2023-03-22 18:41:02 +0100

    Erste variabeln

---
+++ script.js
@@ -0,0 +1,8 @@
+const digStd =document.getElementsByClassName("digital-Std"); 
+const digMin =document.getElementsByClassName("digital-Min"); 
+const digSec =document.getElementsByClassName("digital-Sec"); 
+
+let digitalstd =0;
+let digitalmin =0;
+let digitalsec =0;
+
