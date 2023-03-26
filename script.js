6d36a91487ad09e8d6197b2da535ed34ab59f682
Jens gerwins <Jens.gerwins@gmail.com>
2023-03-26 15:40:53 +0200

    Erste ausführung

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const digStd =document.getElementById("digital-Std"); 
 const digMin =document.getElementById("digital-Min"); 
 const digSec =document.getElementById("digital-Sec"); 
-
+const analogStdZeiger =document.getElementById("std-zeiger");
+const analogMinZeiger =document.getElementById("std-min");
+const analogSecZeiger =document.getElementById("std-sekunden");
+//Digital Uhr
 function DigUhr() {
 const d =new Date();
 let digitalstd =d.getHours();
@@ -11,6 +14,18 @@ let digitalsec =d.getSeconds();
 digStd.innerText = nummerToStr(digitalstd);
 digMin.innerText = nummerToStr(digitalmin);
 digSec.innerText = nummerToStr(digitalsec);
+
+
+//Analoge-Uhr
+
+
+analogStdZeiger.style.setProperty("--stunden", digitalstd * 30 + "deg");
+analogMinZeiger.style.setProperty("--minuten", digitalmin * 6 + "deg");
+analogSecZeiger.style.setProperty("--seconden", digitalsec * 6 + "deg");
+
+
+
+
 
 }
 
